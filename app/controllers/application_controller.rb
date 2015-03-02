6f9e846182df6107710a9133d8294b89e29ef2fe
Stefan Surzycki <stefan.surzycki@gmail.com>
2015-03-02 01:36:26 +0100

    allow iframes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   #protect_from_forgery with: :exception
 
+  after_filter :allow_iframe
+
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end 
 end
