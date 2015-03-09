8f207ea6fe1b89a9f0c2db2fa100fdf555da12a3
Stefan Surzycki <stefan.surzycki@gmail.com>
2015-03-09 11:06:56 +0100

    update referals controller

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -1,6 +1,5 @@
 class ReferralsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :index
-
+  #skip_before_filter :verify_authenticity_token, only: :index
   def index
     logger.info 'hi'
     i = 10
