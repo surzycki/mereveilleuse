69ca36906a5806c65b9ce542ef5bfe14847cecda
Stefan Surzycki <stefan.surzycki@gmail.com>
2015-03-09 12:29:38 +0100

    change raygun

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -3,5 +3,5 @@ Raygun.setup do |config|
   config.filter_parameters = Rails.application.config.filter_parameters
 
   # The default is Rails.env.production?
-  config.enable_reporting = !Rails.env.test?
+  config.enable_reporting = Rails.env.staging? || Rails.env.production?
 end
