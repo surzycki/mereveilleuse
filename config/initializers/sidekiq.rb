c0073aae122a915fa296baa4dc9681898639388c
Stefan Surzycki <stefan.surzycki@gmail.com>
2015-03-31 03:01:04 +0200

    rename images

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "#{ENV['REDIS_URL']}/0", namespace: "sidekiq_app_name_#{Rails.env}" }
+  config.redis = { url: "#{ENV['REDIS_URL']}/0", namespace: "sidekiq_mereveilleuse_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "#{ENV['REDIS_URL']}/0", namespace: "sidekiq_app_name_#{Rails.env}" }
+  config.redis = { url: "#{ENV['REDIS_URL']}/0", namespace: "sidekiq_mereveilleuse_#{Rails.env}" }
 end
