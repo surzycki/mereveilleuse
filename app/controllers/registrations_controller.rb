85fe7f9951f5efaf20d2b5134d95cefd51f3b69a
Stefan Surzycki <stefan.surzycki@gmail.com>
2015-04-14 18:42:47 +0200

    redirection

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < ApplicationController
 
   # GET registrations/new
   def new
-    @requesting_authentication = params[:requesting_authentication]
+    #@requesting_authentication = params[:requesting_authentication]
   end 
 
   def catch_exceptions
