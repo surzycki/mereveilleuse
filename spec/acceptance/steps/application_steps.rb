8f5fd6777b78c857a5b619c930562ef26551e58d
Stefan Surzycki <stefan.surzycki@gmail.com>
2015-06-02 13:18:20 +0200

    update steps

--- spec/acceptance/steps/application_steps.rb
+++ spec/acceptance/steps/application_steps.rb
@@ -63,7 +63,7 @@ module ApplicationSteps
 
   step 'the Facebook :pixel_type conversion pixel :whether_to be fired' do |pixel_type, positive|
     expectation = positive ? :to : :not_to
-    text = "facebook conversion code for #{pixel_type.downcase}"
+    text = "Facebook Conversion Code for #{pixel_type.downcase}"
     
     expect(page.body).send expectation, include(text)
   end
